643ad488785bd23048a833affedc1ba27dfedb62
FRK-SANDKAKE\linda <frk.sandkake@gmail.com>
2023-01-12 16:35:06 +0100

    Updated guitars page, fetching products from guitars.js (array w/guitar objects) via getGuitarCards.js.

---
+++ js/guitars.js
@@ -0,0 +1,82 @@
+const products = [
+    {
+        name: "Adirondack Red",
+        price: 425,
+        image: "../images/products/product1.jpg",
+        id: 1,
+        description: "The Adirondack Red guitar is great. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent hendrerit elit tincidunt nibh imperdiet, sed venenatis mauris rutrum. Cras euismod risus sed tellus porttitor, ut bibendum orci auctor.",
+        featured: true,
+        sales: false
+    },
+    {
+        name: "The Performer",
+        price: 750,
+        image: "../images/products/product2.jpg",
+        id: 2,
+        description: "The Performer guitar is great. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent hendrerit elit tincidunt nibh imperdiet, sed venenatis mauris rutrum. Cras euismod risus sed tellus porttitor, ut bibendum orci auctor.",
+        featured: false,
+        sales: true
+    },
+    {
+        name: "Sessioner",
+        price: 625,
+        image: "../images/products/product3.jpg",
+        id: 3,
+        description: "The Sessioner guitar is great. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent hendrerit elit tincidunt nibh imperdiet, sed venenatis mauris rutrum. Cras euismod risus sed tellus porttitor, ut bibendum orci auctor.",
+        featured: false,
+        sales: false
+    },
+    {
+        name: "Vintage Electric",
+        price: 525,
+        image: "../images/products/product4.jpg",
+        id: 4,
+        description: "The Vintage Electric guitar is great. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent hendrerit elit tincidunt nibh imperdiet, sed venenatis mauris rutrum. Cras euismod risus sed tellus porttitor, ut bibendum orci auctor.",
+        featured: true,
+        sales: true
+    },
+    {
+        name: "The Writer",
+        price: 450,
+        image: "../images/products/product5.jpg",
+        id: 5,
+        description: "The Adirondack Red guitar is great. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent hendrerit elit tincidunt nibh imperdiet, sed venenatis mauris rutrum. Cras euismod risus sed tellus porttitor, ut bibendum orci auctor.",
+        featured: true,
+        sales: false
+    },
+    {
+        name: "Steam Punk",
+        price: 675,
+        image: "../images/products/product6.jpg",
+        id: 6,
+        description: "The Steam Punk guitar is great. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent hendrerit elit tincidunt nibh imperdiet, sed venenatis mauris rutrum. Cras euismod risus sed tellus porttitor, ut bibendum orci auctor.",
+        featured: true,
+        sales: true
+    },
+    {
+        name: "Honky Tonk",
+        price: 680,
+        image: "../images/products/product7.jpg",
+        id: 7,
+        description: "The Honky Tonk guitar is great. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent hendrerit elit tincidunt nibh imperdiet, sed venenatis mauris rutrum. Cras euismod risus sed tellus porttitor, ut bibendum orci auctor.",
+        featured: true,
+        sales: true
+    },
+    {
+        name: "Gold",
+        price: 620,
+        image: "../images/products/product8.jpg",
+        id: 8,
+        description: "The Gold guitar is great. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent hendrerit elit tincidunt nibh imperdiet, sed venenatis mauris rutrum. Cras euismod risus sed tellus porttitor, ut bibendum orci auctor.",
+        featured: true,
+        sales: false
+    },
+    {
+        name: "Ukulele",
+        price: 250,
+        image: "../images/products/product9.jpg",
+        id: 9,
+        description: "The Ukulule is great. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent hendrerit elit tincidunt nibh imperdiet, sed venenatis mauris rutrum. Cras euismod risus sed tellus porttitor, ut bibendum orci auctor.",
+        featured: false
+    },
+]
